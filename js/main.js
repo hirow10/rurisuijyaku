4bbfc4c01e1a6c22f5157a130d5a070e70158fd7
hirow10 <hirow10@gmail.com>
2020-11-03 14:22:17 +0900

    CHANGE modal Window

--- js/main.js
+++ js/main.js
@@ -95,7 +95,18 @@
     var cardsChosen = [];
     var cardsChosenId = [];
     const cardsWon = [];
+    // let optionOneId;
+    // let optionTwoId;
+    let cardA;
+    let cardB;
+    let cardsWonLengthCount = 0;
 
+    // const open = document.getElementById('open');
+    const close = document.getElementById('close');
+    const modal = document.getElementById('modal');
+    const mask = document.getElementById('mask');
+
+    const timer = document.getElementById('timer');
     let startTime;
     let timeoutId;
     let timerStartNum = 0;
@@ -116,29 +127,52 @@
     //check for matches
     function checkForMatch() {
       var cards = document.querySelectorAll('img');
+      // console.log(cards);
       const optionOneId = cardsChosenId[0];
       const optionTwoId = cardsChosenId[1];
+      // optionOneId = cardsChosenId[0];
+      // console.log(optionOneId);
+      // console.log(cards[optionOneId]);
+      // optionTwoId = cardsChosenId[1];
+      // console.log(optionTwoId);
+      // console.log(cards[optionTwoId]);
+
+      cardA = cards[optionOneId];
+      cardB = cards[optionTwoId];
+      // console.log(cardA);
+      // console.log(cardB);
 
       if (optionOneId == optionTwoId) {
-        cards[optionOneId].setAttribute('src', 'images/click.png');
-        cards[optionTwoId].setAttribute('src', 'images/click.png');
-        setTimeout(alert('連打はアカン！'), 200);
-        cards[optionOneId].classList.remove('ura');
-        cards[optionTwoId].classList.remove('ura');
+        // cards[optionOneId].setAttribute('src', 'images/click.png');
+        // cards[optionTwoId].setAttribute('src', 'images/click.png');
+        modalMsg('連打はアカン！');
+        // setTimeout(alert('連打はアカン！'), 200);
+        // turnItOver(cards[optionOneId], cards[optionTwoId]);
+        // cards[optionOneId].classList.remove('ura');
+        // cards[optionTwoId].classList.remove('ura');
         // alert('連打はアカン！');
       }
       else if (cardsChosen[0] === cardsChosen[1]) {
-        alert(`よう見つけたな！ `);
+        modalMsg('よう見つけたな！');
+        // alert(`よう見つけたな！ `);
         // alert(`よう見つけたな！ ${cardArray[optionOneId].name}`)
-        cards[optionOneId].removeEventListener('click', flipCard);
-        cards[optionTwoId].removeEventListener('click', flipCard);
+        // cards[optionOneId].removeEventListener('click', flipCard);
+        // cards[optionTwoId].removeEventListener('click', flipCard);
+        cardA.removeEventListener('click', flipCard);
+        cardB.removeEventListener('click', flipCard);
+        // console.log(cardA);
+        // console.log(cardB);
+        // console.log(cardsWon.length);
         cardsWon.push(cardsChosen);
+        // console.log(cardsWon.length);
       } else {
-        cards[optionOneId].setAttribute('src', 'images/click.png');
-        cards[optionTwoId].setAttribute('src', 'images/click.png');
-        setTimeout(alert('ちゃうで！'), 200);
-        cards[optionOneId].classList.remove('ura');
-        cards[optionTwoId].classList.remove('ura');
+        modalMsg('ちゃうで！');
+        // cards[optionOneId].setAttribute('src', 'images/click.png');
+        // cards[optionTwoId].setAttribute('src', 'images/click.png');
+        // setTimeout(alert('ちゃうで！'), 200);
+        // turnItOver(cards[optionOneId], cards[optionTwoId]);
+        // cards[optionOneId].classList.remove('ura');
+        // cards[optionTwoId].classList.remove('ura');
         // alert('ちゃうで！');
       }
       cardsChosen = [];
@@ -165,7 +199,7 @@
       cardsChosenId.push(cardId);
       this.setAttribute('src', cardArray[cardId].img);
       this.classList.add('ura');
-      // console.log('ura');
+
       if (cardsChosen.length === 2) {
         setTimeout(checkForMatch, 500);
       }
@@ -183,13 +217,53 @@
       }, 10);
     }
 
+    function turnItOver(varCardA, varCardB) {
+      varCardA.classList.remove('ura');
+      varCardB.classList.remove('ura');
+      varCardA.setAttribute('src', 'images/click.png');
+      varCardB.setAttribute('src', 'images/click.png');
+
+      // cards[optionOneId].classList.remove('ura');
+      // cards[optionTwoId].classList.remove('ura');
+
+    }
 
 
+    function modalMsg(msg) {
+      mask.classList.remove('hidden');
+      modal.classList.remove('hidden');
+      modal.textContent = msg;
+      // close.textContent = '閉じる';
+    }
+
+    // open.addEventListener('click', () => {
+    //   mask.classList.remove('hidden');
+    //   modal.classList.remove('hidden');
+    // });
+
+    close.addEventListener('click', () => {
+      mask.classList.add('hidden');
+      modal.classList.add('hidden');
+    });
+
+    mask.addEventListener('click', () => {
+      // turnItOver(cards[optionOneId], cards[optionTwoId]);
+      close.click();
+      console.log(cardsWon.length);
+      // turnItOver(x, y);
+      // turnItOver(cards[optionOneId], cards[optionTwoId]);
+      if (cardsWon.length === cardsWonLengthCount + 1) {
+        cardsWonLengthCount++;
+      } else {
+        turnItOver(cardA, cardB);
+      }
+    });
 
 
-    createBoard()
+    createBoard();
+
+
   })
-
 
 
 
